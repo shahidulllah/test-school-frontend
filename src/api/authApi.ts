6c10c88cce379bc6cc778a86e7e3bb5b3c7d0a4d
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-11 11:51:04 +0600

    auth type and api creation

---
+++ src/api/authApi.ts
@@ -0,0 +1,27 @@
+import type {
+  AuthResponse,
+  LoginRequest,
+  RegisterRequest,
+} from "../types/authType";
+import { baseApi } from "./baseApi";
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation<AuthResponse, LoginRequest>({
+      query: (credentials) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    register: builder.mutation<AuthResponse, RegisterRequest>({
+      query: (data) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = authApi;
