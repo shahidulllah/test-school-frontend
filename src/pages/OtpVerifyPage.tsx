c1d0f119dfab6d8efea42666d2654bfa0e3b9f79
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-13 01:47:33 +0600

    modify

--- src/pages/OtpVerifyPage.tsx
+++ src/pages/OtpVerifyPage.tsx
@@ -1,2 +1,8 @@
-const OtpVerifyPage = () => <div className="p-4">OTP Verification Page</div>;
+const OtpVerifyPage = () => {
+    return (
+        <div className="p-4">
+            OTP Verification Page
+        </div>
+    )
+}
 export default OtpVerifyPage;
